eb94d022cef98816313133d71a6170345956dc84
LambosCode <hameedsleague@gmail.com>
2022-01-22 18:10:32 +0000

    fix rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,12 +9,15 @@ char *rot13(char *s)
 		while ((*s >= 65 && *s <= 90) || (*s >= 97 && *s <= 122))
 		{
 			if ((*s - 13 >= 65 && *s < 90) || *s - 13 >= 97)
+			{
 				*s = *s - 13;
-			else
-				*s = *s + 13;
+				goto lower;
+			}
+			*s = *s + 13;
 			s++;
 			cnt++;
 		}
+lower:
 		s++;
 		cnt++;
 	}
