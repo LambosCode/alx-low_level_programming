ed3c2b68409bd97deef3e1d314ea4a8bcf4c6057
LambosCode <hameedsleague@gmail.com>
2022-04-06 02:55:48 +0000

    rework x4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,10 @@ char *str_concat(char *s1, char *s2)
 	else if (cnt < _strlen(s2))
 		return (s1);
 	if (!s1 && !s2)
-		return ("");
+	{
+		*str = 0;
+		return (str);
+	}
 	else if (!str)
 		return (0);
 
