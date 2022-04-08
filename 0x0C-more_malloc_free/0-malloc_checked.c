a16ccc86560e1e5047df43b30c4972cce51b860b
LambosCode <hameedsleague@gmail.com>
2022-04-08 12:48:49 +0000

    work

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,8 @@
 /**
  * malloc_checked - create memory allocation in heap
  * @b: size
+ *
+ * Return: memory address
  */
 
 void *malloc_checked(unsigned int b)
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	if (!tmp)
-		return (0);
+		return ("");
 
 	return (tmp);
 }
